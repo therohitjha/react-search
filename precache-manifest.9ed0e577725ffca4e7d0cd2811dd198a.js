901ff2b2f5c87c0532d830cb2a09e74ccfce56e9
Rohit <therohitjha@gmail.com>
2020-05-05 09:41:37 +0530

    Updates

---
+++ precache-manifest.9ed0e577725ffca4e7d0cd2811dd198a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d73ca0f9aec90564167f76b427fe2d",
+    "url": "/react-search/index.html"
+  },
+  {
+    "revision": "9968b1bc02a93de2dd42",
+    "url": "/react-search/static/css/main.0f0a2a59.chunk.css"
+  },
+  {
+    "revision": "e3372dab34ed592d1e59",
+    "url": "/react-search/static/js/2.fa07a3cb.chunk.js"
+  },
+  {
+    "revision": "9968b1bc02a93de2dd42",
+    "url": "/react-search/static/js/main.462fefb3.chunk.js"
+  },
+  {
+    "revision": "f5ad28372430b3815de1",
+    "url": "/react-search/static/js/runtime~main.82472245.js"
+  }
+]);
